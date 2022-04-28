3ed468dfcb2904d87064698dc6f592988e1344d2
Maksim Rasporskyi <mraspors@lab3r5s5.42abudhabi.ae>
2022-04-28 15:05:02 +0400

    some fixes

--- push_swap.c
+++ push_swap.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "libft/libft.h"
 
+#pragma region Structs
 typedef struct node {
     int data;
 	int i;
@@ -24,7 +25,9 @@ typedef struct data {
 	int moves;
 	int	size;
 } data_t;
+#pragma endregion Structs
 
+#pragma region main_params_validation
 int		error_non_num(int argc, char **argv)
 {
 	int	i;
@@ -82,7 +85,9 @@ int		check_errors(int argc, char **argv)
 		return (1);
 	return (0);
 }
+#pragma endregion main_params_validation
 
+#pragma region Basic_operatons_with_lists
 node_t	*find_last(node_t *lst)
 {
 	node_t	*node;
@@ -113,23 +118,16 @@ node_t	*find_pre_last(node_t *lst)
 	return (temp);
 }
 
-void	deleteNode(node_t **head_ref)
+void	deleteNode(node_t **head)
 {
-    node_t *temp = *head_ref;
-	node_t *prev;
- 
-	prev = NULL;
-    if(temp != NULL) 
-	{
-        *head_ref = temp->next;
-        free(temp); 
-        return;
-    }
-    prev->next = temp->next;
+	node_t	*temp;
+     
+    temp = *head;    
+    *head = (*head)->next;
     free(temp);
 }
 
-void	push(node_t **head_ref, int i, int new_data, int pos)
+void	push(node_t **head_ref, int new_data, int i, int pos)
 {
 	
     node_t *new_node;
@@ -150,19 +148,21 @@ void	print_list(node_t *a, node_t *b)
 	printf("-------\n");
     while (current_a != NULL || current_b != NULL) {
 		if (current_a == NULL)
-        	printf("- pos: - ::: %d pos: %d\n",current_b->data, current_b->pos);
+        	printf("- pos: - i: - ::: %d pos: %d i: %d\n",current_b->data, current_b->pos, current_b->i);
 		else if (current_b == NULL)
-			printf("%d pos: %d ::: - pos: - \n", current_a->data, current_a->pos);
+			printf("%d pos: %d i: %d ::: - pos: - i: - \n", current_a->data, current_a->pos, current_a->i);
 		else
-			printf("%d pos: %d ::: %d pos: %d \n", current_a->data, current_a->pos, current_b->data, current_b->pos);
+			printf("%d pos: %d i: %d ::: %d pos: %d i: %d \n", current_a->data, current_a->pos, current_a->i, current_b->data, current_b->pos, current_b->i);
 		if (current_a != NULL)
         	current_a = current_a->next;
 		if (current_b != NULL)
 		current_b = current_b->next;
     }
-	printf("   A    ---   B   \n");
+	printf("   A        ---       B   \n");
 }
+#pragma endregion Basic_operatons_with_lists
 
+#pragma region Moves
 void	sa(node_t *a)
 {
 	int		x;
@@ -192,7 +192,7 @@ void	pa(node_t **a, node_t **b)
 	node_t *temp;
 
 	temp = *b;
-	push(a, 0, temp->data, temp->pos);
+	push(a, temp->data, 0, temp->pos);
 	deleteNode(b);
 }
 
@@ -201,7 +201,7 @@ void	pb(node_t **a, node_t **b)
 	node_t *temp;
 
 	temp = *a;
-	push(b, 0, temp->data, temp->pos);
+	push(b, temp->data, 0, temp->pos);
 	deleteNode(a);
 }
 
@@ -268,7 +268,9 @@ void	rrr(node_t **a, node_t **b)
 	rra(a);
 	rrb(b);
 }
+#pragma endregion Moves
 
+#pragma region helpers
 int		check_sort(node_t *head)
 {
 	node_t * current = head;
@@ -296,21 +298,24 @@ void	reset_i(node_t *head)
 	}
 }
 
-void	node_put_pos(node_t *head, int data, int pos)
+int		list_size(node_t *head)
 {
 	node_t	*current;
+	int		i;
 
 	current = head;
+	i = 0;
 	while (current != NULL)
 	{
-		if (current->data == data)
-		{
-			current->pos = pos;
-		}
+		i++;
 		current = current->next;
 	}
+	return (i);
 }
 
+#pragma endregion helpers
+
+#pragma region Finding_Positions
 int		errors_for_pos(int index, int *arr, int l)
 {
 	int	k;
@@ -325,7 +330,7 @@ int		errors_for_pos(int index, int *arr, int l)
 	return (0);
 }
 
-node_t		*find_min_node(node_t *head, int *arr, int arr_el)
+node_t	*find_min_node(node_t *head, int *arr, int arr_el)
 {
 	node_t	*current;
 	node_t	*result;
@@ -359,14 +364,56 @@ void	find_pos(node_t *head, int l)
 	{
 		current = find_min_node(head, arr, i );
 		current->pos = i + 1;
-		printf("cuurent info: data - %d, i - %d\n", current->data, current->i);
 		arr[i] = current->i;
 		i++;
 	}
-	for (int k = 0; k < l; k++)
-		printf("%d ,", arr[k]);
-	printf("\n");
 }
+#pragma endregion Finding_Positions
+
+#pragma region sorting
+
+int		find_node_i(node_t *head, int pos)
+{
+	node_t	*current;
+
+	current = head;
+	while (current != NULL)
+	{
+		if (current->pos == pos)
+			return (current->i);
+		current = current->next;
+	}
+	return (0);
+}
+
+node_t 	*sort(node_t *a, node_t *b, int size)
+{
+	int	i;
+	int	moves;
+
+	i = 0;
+	while (i < size)
+	{
+		moves = find_node_i(a, i + 1);
+		while (moves > 1)
+		{
+			ra(&a);
+			moves--;
+		}
+		pb(&a, &b);
+		reset_i(a);
+		i++;
+	}
+	i = 0;
+	while (i < size)
+	{
+		pa(&a, &b);
+		i++;
+	}
+	return (a);
+}
+
+#pragma endregion sorting
 
 int		main(int argc, char **argv)
 {
@@ -384,11 +431,13 @@ int		main(int argc, char **argv)
 	i = argc - 1;
 	while (i > 0)
 	{
-		push(&a, i, ft_atoi(argv[i]), 0);
+		push(&a, ft_atoi(argv[i]), i, 0);
 		i--;
 	}
+	i = list_size(a);
+	find_pos(a, i);
 	print_list(a, b);
-	find_pos(a, argc - 1);
+	a = sort(a, b, i);
 	print_list(a, b);
 	return (0);
 }
